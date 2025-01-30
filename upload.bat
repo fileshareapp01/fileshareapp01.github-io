e875bd1d83b9c71e335ee06807bcc5aeb91ae36a
fileshareapp01 <krishna22kanth77@gmail.com>
2025-01-30 23:57:51 +0530

    Auto-upload on 30-01-2025 at 23:57:51.10

--- upload.bat
+++ upload.bat
@@ -1,27 +1,15 @@
 @echo off
-REM Set up variables for the local folder and remote repository
-set REPO_DIR=C:\Users\YourName\YourRepo
-set GITHUB_REPO=https://github.com/fileshareapp01/fileshareapp01.github-io.git
+:: Navigate to the local repository folder
+cd "C:\Users\D MANOHAR\fileshareapp01.github-io"
 
-REM Navigate to the repository directory
-cd /d %REPO_DIR%
-
-REM Initialize Git repository if not already initialized
-if not exist ".git" (
-    git init
-    git remote add origin %GITHUB_REPO%
-)
-
-REM Add all files to the staging area
+:: Stage all changes
 git add .
 
-REM Commit changes with a message
-git commit -m "Automatic upload from batch script"
+:: Commit the changes with a timestamp
+git commit -m "Auto-upload on %date% at %time%"
 
-REM Push changes to the remote repository
-git push origin master
+:: Push changes to the GitHub repository
+git push origin main
 
-REM End script
-echo Files uploaded successfully!
-pause
-
+::Exit
+exit
